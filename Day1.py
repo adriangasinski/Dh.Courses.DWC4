21199fdd7bb741cd3fb936c78719dc305cb50a49
adriangasinski <adrian@datahacking.tech>
2019-09-03 23:44:54 +0200

    DataWorkshop Challenge 4 - Day 1

---
+++ Day1.py
@@ -0,0 +1,26 @@
+#%% [markdown]
+# ## DataWorkshop Challenge 4 
+# Cel pierwszego dnia to przygotowanie środowiska
+
+
+
+#%% [markdown]
+# ## Do wykonania w konsoli 
+
+# pip install gensim
+# pip install pyLDAvis
+
+
+
+
+#%%
+import gensim
+import pyLDAvis
+
+#%%
+gensim.__version__
+
+#%%
+pyLDAvis.__version__
+
+#%%
